05975231561e94520b994caab47448e3041eb76b
jrgnicho <jrgnicho@gmail.com>
2013-11-23 12:45:29 -0600

    renamed test_class.cpp to test_state_manager.cpp

---
+++ ogre_basics/src/tests/test_state_manager.cpp
@@ -0,0 +1,37 @@
+/*
+ * test_class.cpp
+ *
+ *  Created on: Aug 13, 2012
+ *      Author: coky
+ */
+#include <state_management/StateManager.h>
+#include <state_types/TestState.h>
+#include <state_types/TestDisplayImageState.h>
+#include <OGRE/Ogre.h>
+int main(int argc,char** argv)
+{
+	std::cout<<"Working directory: "<<argv[0]<<"\n";
+	StateManager *stateManager = StateManager::getSingleton();
+	//stateManager->setup();// change access to setup and cleanup functions
+
+	TestState *testState = (TestState*)TestState::getSingleton();
+	//testState->setup();
+
+	TestDisplayImageState *testImgDisplayState = (TestDisplayImageState*)TestDisplayImageState::getSingleton();
+	//testImgDisplayState->setup();
+
+	// setting next state as the image display
+	testState->setChangeState(testImgDisplayState);
+	testImgDisplayState->setChangeState(testState);
+
+	// setting next state to push
+	testState->setNextState(testImgDisplayState);
+
+	stateManager->start(testState);
+
+	stateManager->cleanup();
+	StateManager::destroySingleton();
+
+	return 0;
+}
+
