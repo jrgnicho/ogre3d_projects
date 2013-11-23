e0386dcd58c525b87c992377e55f9d4d4fafe664
jrgnicho <jrgnicho@gmail.com>
2013-11-23 12:52:12 -0600

    added methods for creating boxes and planes

--- ogre_basics/include/utilities/ShapeDrawer.h
+++ ogre_basics/include/utilities/ShapeDrawer.h
@@ -13,47 +13,64 @@
 
 class ShapeDrawer
 {
+
+public:
 	enum ShapeType
 	{
 		SPHERE = 1,
 		BOX = 2,
 		PIRAMID = 3,
 		CAPSULE = 4,
-		CYLINDER = 5
+		CYLINDER = 5,
+		PLANE = 6
 	};
+
+	typedef boost::shared_ptr<ShapeDrawer> Ptr;
+	typedef ShapeDrawer::Ptr ShapeDrawerPtr;
+	typedef std::map<int,Ogre::MeshPtr> MeshMap;
 
 public:
 	virtual ~ShapeDrawer();
 
-	static ShapeDrawer* getSingleton();
+	static ShapeDrawer::ShapeDrawerPtr getSingleton();
 
-	Ogre::Entity* createEntity(ShapeType shape);
+	Ogre::MeshPtr get_mesh(ShapeType type);
+	Ogre::MeshPtr create_grid_mesh(float width,float height,int x_segments,int y_segments);
 
 	void exportMeshes(std::string path);
 
-	static const std::string RESOURCE_GROUP_NAME;
-	static const std::string DEFAULT_SAVE_PATH;
-	static const std::string MESH_MATERIAL_NAME;
+	static const std::string RESOURCE_GROUP;
+	static const std::string RESOURCE_DIRECTORY;
 	static const std::string MESH_BOX_NAME;
 	static const std::string MESH_SPHERE_NAME;
 	static const std::string MESH_PIRAMID_NAME;
 	static const std::string MESH_CAPSULE_NAME;
 	static const std::string MESH_CYLINDER_NAME;
+	static const std::string MESH_PLANE_NAME;
 	static const std::string SERIALIZED_MESH_EXTENSION;
 
 protected:
 	ShapeDrawer();
 
-	boost::shared_ptr<ShapeDrawer> _InstancePtr;
+	void init();
+	void close();
+
 
 	Ogre::MeshPtr buildBox();
 	Ogre::MeshPtr buildSphere();
 	Ogre::MeshPtr buildCapsule();
 	Ogre::MeshPtr buildCylinder();
 	Ogre::MeshPtr buildPyramid();
+	Ogre::MeshPtr buildPlane(float width,float height,int x_segments = 1,int y_segments = 1,
+			float u_tile = 1.0f,float v_tile = 1.0f);
 
 	static void computeNormals(const std::vector<Ogre::Vector3> &vertices, const std::vector<unsigned int> &faces,
 			std::vector<Ogre::Vector3> &normals);
+
+protected:
+
+	static ShapeDrawer::ShapeDrawerPtr _InstancePtr;
+	MeshMap mesh_map_;
 
 };
 
--- ogre_basics/src/utilities/ShapeDrawer.cpp
+++ ogre_basics/src/utilities/ShapeDrawer.cpp
@@ -6,23 +6,86 @@
  */
 
 #include <utilities/ShapeDrawer.h>
+#include <utilities/MaterialCreator.h>
+#include <OGRE/OgreMeshManager.h>
+#include <OGRE/OgreSubMesh.h>
+#include <boost/assign/std/vector.hpp> // for 'operator+=()'
+#include <boost/assert.hpp>
+
+using namespace boost::assign;
+
+const std::string ShapeDrawer::RESOURCE_GROUP="UserMeshes";
+const std::string ShapeDrawer::RESOURCE_DIRECTORY="user_media/meshes";
+const std::string ShapeDrawer::MESH_BOX_NAME="BoxMesh";
+const std::string ShapeDrawer::MESH_SPHERE_NAME= "SphereMesh";
+const std::string ShapeDrawer::MESH_PIRAMID_NAME = "PiramidMesh";
+const std::string ShapeDrawer::MESH_CAPSULE_NAME = "CapsuleMesh";
+const std::string ShapeDrawer::MESH_CYLINDER_NAME = "CylinderMesh";
+const std::string ShapeDrawer::MESH_PLANE_NAME = "PlaneMesh";
+const std::string ShapeDrawer::SERIALIZED_MESH_EXTENSION = ".mesh";
+
+ShapeDrawer::ShapeDrawerPtr ShapeDrawer::_InstancePtr;
 
 ShapeDrawer::ShapeDrawer() {
 	// TODO Auto-generated constructor stub
+	init();
 
 }
 
 ShapeDrawer::~ShapeDrawer() {
 	// TODO Auto-generated destructor stub
+	close();
 }
 
-ShapeDrawer* ShapeDrawer::getSingleton()
+ShapeDrawer::ShapeDrawerPtr ShapeDrawer::getSingleton()
 {
-	if(_InstancePtr == NULL)
+	if(!_InstancePtr)
 	{
-		_InstancePtr = boost::make_shared<ShapeDrawer>();
+		_InstancePtr = ShapeDrawerPtr(new ShapeDrawer());
 	}
-	return _InstancePtr.get();
+	return _InstancePtr;
+}
+
+void ShapeDrawer::init()
+{
+	// create resource group
+	std::cout<<"Creating ShapeDrawer Resource Group"<<std::endl;
+	Ogre::ResourceGroupManager& r = Ogre::ResourceGroupManager::getSingleton();
+	r.createResourceGroup(RESOURCE_GROUP);
+	r.addResourceLocation(RESOURCE_DIRECTORY,"FileSystem",RESOURCE_GROUP);
+	r.initialiseResourceGroup(RESOURCE_GROUP);
+	r.loadResourceGroup(RESOURCE_GROUP);
+
+	// creting meshes
+	std::cout<<"Creating Meshes"<<std::endl;
+	mesh_map_ = MeshMap();
+	mesh_map_.insert(std::make_pair((int)ShapeDrawer::BOX,buildBox()));
+	mesh_map_.insert(std::make_pair((int)ShapeDrawer::PLANE,buildPlane(1.0,1.0,1,1,1,1)));
+
+	std::cout<<"Completed ShapeDrawer initialization"<<std::endl;
+}
+
+void ShapeDrawer::close()
+{
+	std::cout<<"Destroying ShapeDrawer Resource Group "<<RESOURCE_GROUP<<"\n";
+	Ogre::ResourceGroupManager::getSingleton().destroyResourceGroup(RESOURCE_GROUP);
+}
+
+Ogre::MeshPtr ShapeDrawer::get_mesh(ShapeType type)
+{
+	Ogre::MeshPtr m;
+	if(mesh_map_.count((int)type)> 0)
+	{
+		m = mesh_map_[(int)type];
+	}
+
+	return m;
+}
+
+void ShapeDrawer::exportMeshes(std::string path)
+{
+	std::cout<<"Not implemented"<<std::endl;
+	return;
 }
 
 Ogre::MeshPtr ShapeDrawer::buildBox()
@@ -35,7 +98,8 @@ Ogre::MeshPtr ShapeDrawer::buildBox()
 	const size_t numVerticesPerFace = 3;
 
 	// creating vertices
-	std::vector<Ogre::Vector3> vertices {Vector3(-0.5f,-0.5f,-0.5f),
+	std::vector<Ogre::Vector3> vertices;
+	vertices += Vector3(-0.5f,-0.5f,-0.5f),
 		  Vector3(+0.5f,-0.5f,-0.5f),
 		  Vector3(-0.5f,+0.5f,-0.5f),
 		  Vector3(+0.5f,+0.5f,-0.5f),
@@ -50,19 +114,22 @@ Ogre::MeshPtr ShapeDrawer::buildBox()
 		  Vector3(-0.5f,-0.5f,+0.5f),
 		  Vector3(+0.5f,-0.5f,+0.5f),
 		  Vector3(-0.5f,+0.5f,+0.5f),
-		  Vector3(+0.5f,+0.5f,+0.5f)};
+		  Vector3(+0.5f,+0.5f,+0.5f);
 
 	// faces array (every three elements correspond to a single triangle)
-	std::vector<unsigned int> faces { 4, 6, 2, 2, 0, 4, 1, 3, 7,7, 5, 1, 8, 9, 13, 13, 12, 8,
-		10, 14, 15, 15,11, 10, 8, 10, 11, 11, 9, 8, 4, 5, 7, 7, 6, 4};
+	std::vector<unsigned int> faces;
+	faces += 4, 6, 2, 2, 0, 4, 1, 3, 7,7, 5, 1, 8, 9, 13, 13, 12, 8,
+		10, 14, 15, 15,11, 10, 8, 10, 11, 11, 9, 8, 4, 5, 7, 7, 6, 4;
 
 	// normals array
 	std::vector<Vector3> normals;
 	computeNormals(vertices,faces,normals);
 
 	// building manual object
+
 	ManualObject obj(MESH_BOX_NAME);
-	obj.begin(MESH_MATERIAL_NAME,RenderOperation::OT_TRIANGLE_LIST,ShapeDrawer::RESOURCE_GROUP_NAME);
+	Ogre::MaterialPtr m_ptr = MaterialCreator::get_singleton()->get_material(MaterialCreator::BLUE);
+	obj.begin(m_ptr->getName(),RenderOperation::OT_TRIANGLE_LIST,ShapeDrawer::RESOURCE_GROUP);
 
 	// adding mesh data to object
 	for(unsigned int i = 0; i < faces.size(); i+=3)
@@ -73,6 +140,7 @@ Ogre::MeshPtr ShapeDrawer::buildBox()
 			Vector3 &n = normals[(int)((float)i/(float)numVerticesPerFace)];
 
 			obj.position(v.x,v.y,v.z);
+			//obj.colour(Ogre::ColourValue(0,0,1.0f,1.0f));
 			obj.normal(n);
 			obj.textureCoord(v.x/100.0f,v.z/100.0f);
 		}
@@ -84,9 +152,62 @@ Ogre::MeshPtr ShapeDrawer::buildBox()
 
 	// creating mesh
 	std::string meshName = MESH_BOX_NAME + SERIALIZED_MESH_EXTENSION;
-	Ogre::MeshPtr meshPtr = obj.convertToMesh(meshName,ShapeDrawer::RESOURCE_GROUP_NAME);
+	Ogre::MeshPtr meshPtr = obj.convertToMesh(meshName,ShapeDrawer::RESOURCE_GROUP);
 	return meshPtr;
 
+}
+
+Ogre::MeshPtr ShapeDrawer::buildPlane(float width,float height,int x_segments,int y_segments,
+		float u_tile ,float v_tile)
+{
+
+	// create name
+	std::stringstream ss;
+	int counter = 0;
+	std::string name = MESH_PLANE_NAME + "0" + SERIALIZED_MESH_EXTENSION;
+	while(Ogre::MeshManager::getSingletonPtr()->resourceExists(name))
+	{
+		ss.str("");
+		ss<<MESH_PLANE_NAME<<++counter<<SERIALIZED_MESH_EXTENSION;
+		name = ss.str();
+	}
+
+	Ogre::Plane plane = Ogre::Plane(0.0f,0.0f,1.0f,0.0f);
+	Ogre::MeshPtr mesh_ptr = Ogre::MeshManager::getSingleton().createPlane(name,RESOURCE_GROUP,
+			plane,width,height,x_segments,y_segments,true,1,u_tile,v_tile,Ogre::Vector3::UNIT_Y);
+
+	return mesh_ptr;
+
+}
+
+Ogre::MeshPtr ShapeDrawer::create_grid_mesh(float width,float height,int x_segments,int y_segments)
+{
+
+	// create name
+	std::stringstream ss;
+	int counter = 0;
+	std::string name = MESH_PLANE_NAME + "0";
+	while(Ogre::MeshManager::getSingletonPtr()->resourceExists(name))
+	{
+		ss.str("");
+		ss<<MESH_PLANE_NAME<<++counter;
+		name = ss.str();
+	}
+
+	Ogre::MeshPtr mesh_ptr = buildPlane(width,height,x_segments,y_segments,1,1);
+
+	// applying material to all submeshes
+
+	Ogre::Mesh::SubMeshIterator iter= mesh_ptr->getSubMeshIterator();
+	while(iter.hasMoreElements())
+	{
+		Ogre::SubMesh* sm = iter.getNext();
+		sm->setMaterialName(MaterialCreator::get_singleton()->get_material(MaterialCreator::WIREFRAME_GRAY)->getName(),
+				MaterialCreator::RESOURCE_GROUP);
+	}
+	mesh_ptr->updateMaterialForAllSubMeshes();
+
+	return mesh_ptr;
 }
 
 Ogre::MeshPtr ShapeDrawer::buildSphere()
@@ -109,14 +230,27 @@ Ogre::MeshPtr ShapeDrawer::buildCapsule()
 	return meshPtr;
 }
 
-Ogre::MeshPtr ShapeDrawer::buildCylinder()
-{
-	Ogre::MeshPtr meshPtr;
-	return meshPtr;
-}
 
 Ogre::MeshPtr ShapeDrawer::buildPyramid()
 {
 	Ogre::MeshPtr meshPtr;
 	return meshPtr;
+}
+
+void ShapeDrawer::computeNormals(const std::vector<Ogre::Vector3> &vertices, const std::vector<unsigned int> &faces,
+		std::vector<Ogre::Vector3> &normals)
+{
+
+	// creates 2 vectors from the vertices of each face and computes the normal vector from the cross-product of the two
+	Ogre::Vector3 vec1 = Ogre::Vector3::ZERO;
+	Ogre::Vector3 vec2= Ogre::Vector3::ZERO;
+	Ogre::Vector3 normal= Ogre::Vector3::ZERO;
+	for(int i = 0; i < faces.size(); i +=3)
+	{
+		normal = Ogre::Vector3::ZERO;
+		vec1 = vertices[i+1] - vertices[i];
+		vec2 = vertices[i+2] - vertices[i+1];
+		normal = vec2.crossProduct(vec1);
+		normals.push_back(normal);
+	}
 }
