57b0c15f0b89d36d54f5865b54c1e07dd6c1817c
jrgnicho <jrgnicho@gmail.com>
2013-11-24 14:30:18 -0600

    added header file for TestSceneState class

---
+++ ogre_basics/include/state_types/TestSceneState.h
@@ -0,0 +1,38 @@
+/*
+ * TestSceneState.h
+ *
+ *  Created on: Nov 23, 2013
+ *      Author: ros developer 
+ */
+
+#ifndef TESTSCENESTATE_H_
+#define TESTSCENESTATE_H_
+
+#include <state_types/TestState.h>
+
+class TestSceneState : public TestState
+{
+public:
+	typedef boost::shared_ptr<TestSceneState> Ptr;
+
+public:
+	virtual ~TestSceneState();
+
+	static StateInterface* getSingleton();
+
+public:
+
+	static const std::string STATE_NAME;
+
+protected:
+	TestSceneState();
+	virtual void setupSceneComponents();
+	virtual void setupCameraControllers();
+	virtual void cleanupSceneComponents();
+
+protected:
+
+	std::vector<Ogre::Entity*> entities_;
+};
+
+#endif /* TESTSCENESTATE_H_ */
