8fb01aaf14070c212fae6de6f6ed622f0994ef66
jrgnicho <jrgnicho@gmail.com>
2013-11-29 15:07:55 -0600

    improved material creation

--- ogre_basics/src/utilities/MaterialCreator.cpp
+++ ogre_basics/src/utilities/MaterialCreator.cpp
@@ -37,11 +37,11 @@ void MaterialCreator::init()
 	// create all materials here
 	material_map_ = MaterialMap();
 
-	float brigtness_reduction = 0.8f;
-	Ogre::ColourValue diffuse_color = Ogre::ColourValue(0,0,0.8f,1);
+	float brigtness_reduction = 0.6f;
+	Ogre::ColourValue diffuse_color = Ogre::ColourValue(0.05f,0.05f,1,1);
 	Ogre::ColourValue ambient_color = brigtness_reduction*diffuse_color;
 	ambient_color.a = 1;
-	Ogre::ColourValue specular_color = Ogre::ColourValue(1,1,1,1);
+	Ogre::ColourValue specular_color = Ogre::ColourValue(0.8f,0.8f,0.8f,1);
 	material_map_.insert(std::make_pair(BLUE,
 			build_solid_material(std::string("BlueMaterial"),diffuse_color,ambient_color,specular_color)->getName()));
 
@@ -51,8 +51,8 @@ void MaterialCreator::init()
 	material_map_.insert(std::make_pair(YELLOW,
 			build_solid_material(std::string("YellowMaterial"),diffuse_color,ambient_color,specular_color)->getName()));
 
-	diffuse_color = Ogre::ColourValue(1.0f,0,0,1.0f);
-	ambient_color = brigtness_reduction*diffuse_color;ambient_color.a = 1;
+	diffuse_color = Ogre::ColourValue(0.8f,0.05f,0.05f,1.0f);
+	ambient_color = Ogre::ColourValue(0.3f,0.1f,0.1f,1.0f);
 	material_map_.insert(std::make_pair(RED,
 			build_solid_material(std::string("RedMaterial"),diffuse_color,ambient_color,specular_color)->getName()));
 
@@ -75,21 +75,26 @@ void MaterialCreator::init()
 	material_map_.insert(std::make_pair(WIREFRAME_WHITE,
 			build_wireframe_material(std::string("WireframeWhiteMaterial"),diffuse_color,diffuse_color,diffuse_color)->getName()));
 
-	diffuse_color = Ogre::ColourValue(192.0f/255.0f,192.0f/255.0f,192.0f/255.0f,1.0f);
+	diffuse_color = Ogre::ColourValue(100.0f/255.0f,100.0f/255.0f,100.0f/255.0f,1.0f);
 	material_map_.insert(std::make_pair(WIREFRAME_GRAY,
 			build_wireframe_material(std::string("WireframeGrayMaterial"),diffuse_color,diffuse_color,diffuse_color)->getName()));
+
+	diffuse_color = Ogre::ColourValue(1,1,1,0);
+	material_map_.insert(std::make_pair(WIREFRAME_TRANSPARENT,
+			build_wireframe_material(std::string("WireframeTransparent"),diffuse_color,diffuse_color,diffuse_color)->getName()));
+
 
 	diffuse_color = Ogre::ColourValue(1.0f,0,0,0.4f);
 	ambient_color = brigtness_reduction*diffuse_color;ambient_color.a = 0.4f;
 	specular_color = Ogre::ColourValue(1,1,1,20);
 	material_map_.insert(std::make_pair(TRANSPARENT_RED,
-			build_transparent_material(std::string("TransparentRedMaterial"),diffuse_color,ambient_color,specular_color)->getName()));
+			build_transparent_material(std::string("TransparentRedMaterial"),diffuse_color,ambient_color)->getName()));
 
 	diffuse_color = Ogre::ColourValue(0,0,1,0.4f);
 	ambient_color = brigtness_reduction*diffuse_color;ambient_color.a = 0.4f;
 	specular_color = Ogre::ColourValue(1,1,1,20);
 	material_map_.insert(std::make_pair(TRANSPARENT_BLUE,
-			build_transparent_material(std::string("TransparentBlueMaterial"),diffuse_color,ambient_color,specular_color)->getName()));
+			build_transparent_material(std::string("TransparentBlueMaterial"),diffuse_color,ambient_color)->getName()));
 
 	std::cout<<"Completed MaterialCreator initialization"<<std::endl;
 }
@@ -100,6 +105,32 @@ void MaterialCreator::close()
 
 	std::cout<<"Destroying Material Resource Group "<<RESOURCE_GROUP<<"\n";
 	Ogre::ResourceGroupManager::getSingleton().destroyResourceGroup(RESOURCE_GROUP);
+}
+
+Ogre::MaterialPtr MaterialCreator::create_texture_material(std::string image_name,float utile,float vtile,float alpha)
+{
+	static int img_counter = 0;
+	Ogre::MaterialManager& material_mngr = Ogre::MaterialManager::getSingleton();
+
+	// creating name
+	std::stringstream ss;
+	ss<<"MaterialTexture"<<img_counter<<"_u"<<int(utile)<<"_v"<<int(vtile);
+	Ogre::MaterialPtr m = material_mngr.create(ss.str(),RESOURCE_GROUP);
+
+	Ogre::Technique* tech = m->getTechnique(0);
+	Ogre::Pass* pass = tech->getPass(0);
+	pass->setLightingEnabled(true);
+	pass->setSceneBlending(Ogre::SBT_TRANSPARENT_ALPHA);
+	//pass->setSelfIllumination(Ogre::ColourValue(1,1,1,alpha));
+	pass->createTextureUnitState(image_name);
+	Ogre::TextureUnitState* texture_unit = pass->getTextureUnitState(0);
+	texture_unit->setTextureUScale(utile);
+	texture_unit->setTextureVScale(vtile);
+	texture_unit->setAlphaOperation(Ogre::LBX_SOURCE1,Ogre::LBS_MANUAL,Ogre::LBS_MANUAL,alpha,alpha);
+
+	img_counter++;
+
+	return m;
 }
 
 Ogre::MaterialPtr MaterialCreator::build_solid_material(std::string name,Ogre::ColourValue &diffuse,
@@ -116,7 +147,8 @@ Ogre::MaterialPtr MaterialCreator::build_solid_material(std::string name,Ogre::C
 	first_pass->setDiffuse(diffuse.r,diffuse.g,diffuse.b,diffuse.a);
 	first_pass->setAmbient(ambient.r,ambient.g,ambient.b);
 	first_pass->setSpecular(specular.r,specular.g,specular.b,specular.a);
-	//first_pass->setShininess(64.0f);
+	first_pass->setShininess(96.0f);
+	//first_pass->setSelfIllumination(diffuse);
 	first_pass->setSelfIllumination(Ogre::ColourValue(0,0,0,1));
 
 	return m;
@@ -124,7 +156,7 @@ Ogre::MaterialPtr MaterialCreator::build_solid_material(std::string name,Ogre::C
 }
 
 Ogre::MaterialPtr MaterialCreator::build_transparent_material(std::string name,Ogre::ColourValue &diffuse,
-		Ogre::ColourValue &ambient, Ogre::ColourValue &specular)
+		Ogre::ColourValue &ambient)
 {
 
 	Ogre::MaterialManager& material_mngr = Ogre::MaterialManager::getSingleton();
@@ -136,17 +168,16 @@ Ogre::MaterialPtr MaterialCreator::build_transparent_material(std::string name,O
 	first_pass->setLightingEnabled(true);
 	first_pass->setDiffuse(diffuse.r,diffuse.g,diffuse.b,diffuse.a);
 	first_pass->setAmbient(ambient.r,ambient.g,ambient.b);
-	first_pass->setSpecular(specular.r,specular.g,specular.b,specular.a);
-	//first_pass->setShininess(64.0f);
-	first_pass->setSelfIllumination(Ogre::ColourValue(0,0,0,1));
-	first_pass->setSceneBlending(Ogre::SBT_TRANSPARENT_COLOUR);
+	first_pass->setDepthWriteEnabled(false);
+	first_pass->setSelfIllumination(diffuse);
+	first_pass->setSceneBlending(Ogre::SBT_TRANSPARENT_ALPHA);
 
 	return m;
 
 }
 
 Ogre::MaterialPtr MaterialCreator::build_wireframe_material(std::string name,Ogre::ColourValue &diffuse,
-		Ogre::ColourValue &ambient, Ogre::ColourValue &specular)
+		Ogre::ColourValue &ambient, Ogre::ColourValue &specular,Ogre::ColourValue edge_color)
 {
 
 	Ogre::MaterialManager& material_mngr = Ogre::MaterialManager::getSingleton();
@@ -155,15 +186,21 @@ Ogre::MaterialPtr MaterialCreator::build_wireframe_material(std::string name,Ogr
 
 	Ogre::Technique* first_technique = m->getTechnique(0);
 	Ogre::Pass* first_pass = first_technique->getPass(0);
+	first_pass->setLightingEnabled(true);
 	first_pass->setDiffuse(diffuse.r,diffuse.g,diffuse.b,diffuse.a);
 	first_pass->setAmbient(ambient.r,ambient.g,ambient.b);
-	first_pass->setSpecular(specular.r,specular.g,specular.b,specular.a);
+	first_pass->setDepthWriteEnabled(false);
+	first_pass->setSceneBlending(Ogre::SBT_TRANSPARENT_ALPHA);
 	first_pass->setShininess(64.0f);
-	first_pass->setSelfIllumination(diffuse);
-	first_pass->setPolygonMode(Ogre::PM_WIREFRAME);
+	first_pass->setSelfIllumination(Ogre::ColourValue(0,0,0,1));
+
+	Ogre::Pass* second_pass = first_technique->createPass();
+	//second_pass->setLightingEnabled(false);
+	second_pass->setDiffuse(edge_color);
+	second_pass->setAmbient(edge_color);
+	second_pass->setPolygonMode(Ogre::PM_WIREFRAME);
 
 	return m;
-
 }
 
 Ogre::MaterialPtr MaterialCreator::get_material(Colors c)
