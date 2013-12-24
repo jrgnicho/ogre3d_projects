8a8610cad662954aea9561d25290498b9f7b14b0
jrgnicho <jrgnicho@gmail.com>
2013-12-24 10:49:06 -0600

    completed inherited virtual methods, this class loads an entire scene from a .scene file imported from bullet

--- ogre_basics/include/state_types/TestSceneLoader.h
+++ ogre_basics/include/state_types/TestSceneLoader.h
@@ -11,15 +11,29 @@
 #include <state_types/TestState.h>
 #include <OGRE/OgreOverlay.h>
 #include <OGRE/OgreTextAreaOverlayElement.h>
+#include <boost/shared_ptr.hpp>
 
 class TestSceneLoader :public TestState {
+
 public:
 
 	virtual ~TestSceneLoader();
 
+	static StateInterface* getSingleton();
+
 protected:
+
 	TestSceneLoader();
+	virtual void setupSceneComponents();
+	virtual void setupCameraControllers();
+	virtual void cleanupSceneComponents();
+
+protected:
+
+	static const std::string SCENE_RESOURCE;
+	static const std::string STATE_NAME;
 
 };
+typedef boost::shared_ptr<TestSceneLoader> TestSceneLoaderPtr;
 
 #endif /* TESTSCENELOADER_H_ */
