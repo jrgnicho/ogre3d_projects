6cc9c98858b05b7912d46442b61536da505a647e
jrgnicho <jrgnicho@gmail.com>
2014-06-17 11:40:29 -0500

    reads console input arguments in order to find configuration files

--- ogre_basics/src/tests/test_material_creator.cpp
+++ ogre_basics/src/tests/test_material_creator.cpp
@@ -8,12 +8,31 @@
 #include <utilities/MaterialCreator.h>
 #include <OGRE/Ogre.h>
 
+
+const std::string CONFIGURATION_FILES_DIRECTORY="config/test_setup/";
+const std::string RESOURCE_FILE_NAME="resources.cfg";
+const std::string PLUGINS_FILE_NAME = "plugins.cfg";
+const std::string OGRE_CONFIG_FILE_NAME = "ogre.cfg";
+const std::string HELP_TEXT="test_material_creator program";
+
+
 int main(int argc,char** argv)
 {
-	StateManager* sm_ptr = StateManager::getSingleton();
-	MaterialCreator::Ptr mc_ptr = MaterialCreator::get_singleton();
+	// ogre parameter setup
+	application_parameters::OgreParameters parameters;
+	parameters.ConfigFile = OGRE_CONFIG_FILE_NAME;
+	parameters.ResourcesFile = RESOURCE_FILE_NAME;
+	parameters.PluginsFile = PLUGINS_FILE_NAME;
+	parameters.ParentDirectory = CONFIGURATION_FILES_DIRECTORY;
+	parameters.ConsoleLogging = true;
 
-	sm_ptr->destroySingleton();
+	if(application_parameters::utils::parse_console_parameters(argc,argv,HELP_TEXT,parameters,parameters))
+	{
+		StateManager* sm_ptr = StateManager::init(parameters);
+		MaterialCreator::Ptr mc_ptr = MaterialCreator::get_singleton();
+
+		sm_ptr->destroySingleton();
+	}
 
 	return 0;
 }
